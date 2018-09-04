a265667d296e0fb4e51108cd29f4e61291432d6b
YongMin Choi <stringsorbit@hotmail.com>
2018-09-04 20:28:47 +0900

    3장. 모두를 위한 평등

--- CSharpTDD/Dollar.cs
+++ CSharpTDD/Dollar.cs
@@ -11,9 +11,17 @@ namespace CSharpTDD
             this.amount = amount;
         }
 
+        public override bool Equals(object obj)
+        {
+            Dollar dollar = (Dollar)obj;
+            return amount == dollar.amount;
+         }
+
         public Dollar Times(int multiplier)
         {
             return new Dollar(amount * multiplier);
         }
+
+
     }
 }
